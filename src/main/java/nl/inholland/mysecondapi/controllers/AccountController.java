6e2043bdc360e5d1efab5f271753e63aa376d618
Romy <701224@student.inholland.nl>
2025-05-23 22:20:49 +0200

    added AccountDTO met model mapper

--- src/main/java/nl/inholland/mysecondapi/controllers/AccountController.java
+++ src/main/java/nl/inholland/mysecondapi/controllers/AccountController.java
@@ -1,9 +1,13 @@
 package nl.inholland.mysecondapi.controllers;
 
 import nl.inholland.mysecondapi.models.Account;
+import nl.inholland.mysecondapi.models.dto.AccountDTO;
 import nl.inholland.mysecondapi.services.AccountService;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.config.Configuration;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
 
 import java.util.List;
 @RestController
@@ -11,19 +15,24 @@ import java.util.List;
 public class AccountController {
 
     private final AccountService accountService;
+    ModelMapper modelMapper;
 
-    public AccountController(AccountService accountService) {this.accountService = accountService;}
+    public AccountController(AccountService accountService) {
+        this.accountService = accountService;
+        modelMapper = new ModelMapper();
+        modelMapper.getConfiguration().setFieldMatchingEnabled(true).setFieldAccessLevel(Configuration.AccessLevel.PRIVATE);
+    }
 
-    @GetMapping
+    @GetMapping // implement model mapper
     public ResponseEntity<List<Account>> getAllAccounts() {return ResponseEntity.ok(this.accountService.getAllAcounts());}
 
     @GetMapping("/{id}")
-    public ResponseEntity<Account> getAccountById(@PathVariable Long id) {
-        //gebruik model maper
+    public ResponseEntity<AccountDTO> getAccountById(@PathVariable Long id) {
         return accountService.getAccountById(id)
-                .map(ResponseEntity::ok)
+                .map(account -> ResponseEntity.ok(modelMapper.map(account, AccountDTO.class)))
                 .orElse(ResponseEntity.notFound().build());
     }
+
 
     @PostMapping
     public ResponseEntity<Account> createAccount(@RequestBody Account account) {return ResponseEntity.ok(this.accountService.createAccount(account));}
---
+++ src/main/java/nl/inholland/mysecondapi/models/dto/AccountDTO.java
@@ -0,0 +1,21 @@
+package nl.inholland.mysecondapi.models.dto;
+
+import lombok.Data;
+import nl.inholland.mysecondapi.models.enums.AccountStatus;
+import nl.inholland.mysecondapi.models.enums.AccountType;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+public class AccountDTO {
+    private Long id;
+    private String IBAN;
+    private BigDecimal balance;
+    private BigDecimal accountLimit;
+    private AccountType type;
+    private AccountStatus status;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+    private Long ownerId; // to reference the User entity by ID
+}
